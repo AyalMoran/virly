b78d1cdb52f45a9836fa40e4d0078fb826a047b8
ayal.moran <moranayal@gmail.com>
2026-01-22 14:37:58 +0200

    init rule of three

---
+++ cpp/other/rule_of_three/rule_of_three.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+struct X
+{
+    explicit X(int);
+    ~X();
+    void Foo();
+    void Bar() const;
+
+    int m_a;
+    int* m_p;
+};
+
+X::X(int a_) : m_a(a_), m_p(new int(a_))
+{
+}
+
+X::~X()
+{
+    delete m_p;
+    m_p = 0;
+}
+
+void X::Foo()
+{
+    ++m_a;
+    --(*m_p);
+}
+
+void X::Bar() const
+{
+    std::cout << m_a << std::endl;
+    std::cout << *m_p << std::endl;
+    std::cout << m_p << std::endl;
+    // m_a = 0; //--1---
+    // m_p = 0; //--2---
+    // *m_p = 0; //--3---
+    // Foo(); //--5---
+}
+
+void Fifi(const X& x_)
+{
+    // x_.Foo(); //--4---
+    x_.Bar();
+}
+
+int main()
+{
+    X x1(1);
+    X x2(x1); // double free at destructor
+    /*
+    The compiler will generate a copy constructor that does a shallow copy of the pointers and when x2 destruct it will free the pointer `m_p` which will result in double free when x1 is destroyed.
+    */
+    X x3 = x2;  
+    /*
+    The compiler 
+
+    */
+    x1.Foo();
+    Fifi(x1);
+
+    return 0;
+}
