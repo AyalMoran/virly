f766ceb37be21b4594d5626d9da9f7b3ff9053b5
ayal.moran <moranayal@gmail.com>
2026-02-10 12:23:36 +0200

    batman ipc ping pong

---
+++ system_programming/inc/IPCPingPong.h
@@ -0,0 +1,12 @@
+/**************************************************************
+ * File    : IPCPingPong.h
+ * Author  : Ayal Moran
+ * Reviewer: 
+ * Date    : 
+**************************************************************/
+#ifndef _ILRD_IPCPINGPONG_H
+#define _ILRD_IPCPINGPONG_H
+
+/*Declarations for IPCPingPong*/
+
+#endif /* _ILRD_IPCPINGPONG_H */
