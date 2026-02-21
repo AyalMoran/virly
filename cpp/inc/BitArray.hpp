0744e850d2071042a5cfc620d2238c0cb4867332
ayal.moran <moranayal@gmail.com>
2026-02-21 15:33:03 +0200

    bit array init

---
+++ cpp/inc/BitArray.hpp
@@ -0,0 +1,12 @@
+/**************************************************************
+ * File    : BitArray.hpp
+ * Author  : Ayal Moran
+ * Reviewer: 
+ * Date    : 
+**************************************************************/
+#ifndef _ILRD_BITARRAY_HPP
+#define _ILRD_BITARRAY_HPP
+
+/*Declarations for BitArray*/
+
+#endif /* _ILRD_BITARRAY_HPP */
