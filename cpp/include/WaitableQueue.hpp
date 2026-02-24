d3d67306d3f61c9cb1d4748ed09cc9c1532c0160
ayal.moran <moranayal@gmail.com>
2026-02-24 15:34:34 +0200

    batman

---
+++ cpp/include/WaitableQueue.hpp
@@ -0,0 +1,12 @@
+/**************************************************************
+ * File    : WaitableQueue.hpp
+ * Author  : Ayal Moran
+ * Reviewer: 
+ * Date    : 
+**************************************************************/
+#ifndef _ILRD_WAITABLEQUEUE_HPP
+#define _ILRD_WAITABLEQUEUE_HPP
+
+/*Declarations for WaitableQueue*/
+
+#endif /* _ILRD_WAITABLEQUEUE_HPP */
