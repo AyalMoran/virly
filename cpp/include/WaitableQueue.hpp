f58dc6d40a469e68ea59cb1732f5c5bc0f0931ee
ayal.moran <moranayal@gmail.com>
2026-02-24 20:27:23 +0200

    Waitable queue fixed and tests passed.

--- cpp/include/WaitableQueue.hpp
+++ cpp/include/WaitableQueue.hpp
@@ -8,56 +8,71 @@
 #ifndef _ILRD_WAITABLEQUEUE_HPP
 #define _ILRD_WAITABLEQUEUE_HPP
 #include <chrono>
-#include <queue>
-#include <mutex>
 #include <condition_variable>
+#include <mutex>
+#include <queue>
 
-/*Declarations for WaitableQueue*/
 namespace ilrd
 {
 template <typename T, class CONTAINER = std::queue<T>> class WaitableQueue
 {
-  private:
-    std::mutex m_mutex;
-    std::condition_variable m_cond_var;
-    CONTAINER m_q;
-
   public:
     WaitableQueue();
 
     void Push(const T& to_push);
     void Pop(T& out_param);
-    bool Pop(std::size_t time_out_ms, T& out_param);
+    bool Pop(std::chrono::milliseconds time_out_ms, T& out_param);
+
+    // CODE_REVIEW: add documentation on the fragility of this bool's integrity
+    // in a multithreaded application
     bool IsEmpty();
+
+  private:
+    std::timed_mutex m_mutex;
+    std::condition_variable_any m_cond_var;
+    CONTAINER m_q;
 };
+
 template <typename T, class CONTAINER>
 WaitableQueue<T, CONTAINER>::WaitableQueue() : m_mutex(), m_cond_var(), m_q()
 {
-
 }
 
-template <typename T, class CONTAINER> 
+template <typename T, class CONTAINER>
 void WaitableQueue<T, CONTAINER>::Push(const T& to_push)
-{
-    std::unique_lock<std::mutex> lock(m_mutex);
-    m_q.push(to_push);
+{ // CODE_REVIEW: use scope reolution
+    {
+        std::unique_lock<std::timed_mutex> lock(m_mutex);
+        m_q.push(to_push);
+    }
     m_cond_var.notify_one();
 }
+
 template <typename T, class CONTAINER>
 void WaitableQueue<T, CONTAINER>::Pop(T& out_param)
 {
-  std::unique_lock<std::mutex> lock(m_mutex);
-  m_cond_var.wait(lock, [this]() { return !(this->m_q.empty()); });
+    std::unique_lock<std::timed_mutex> lock(m_mutex);
+    m_cond_var.wait(lock, [this]() { return !(this->m_q.empty()); });
     out_param = m_q.front();
     m_q.pop();
     return;
 }
 
 template <typename T, class CONTAINER>
-bool WaitableQueue<T, CONTAINER>::Pop(std::size_t time_out_ms, T& out_param)
+bool WaitableQueue<T, CONTAINER>::Pop(std::chrono::milliseconds time_out_ms,
+                                      T& out_param)
 {
-    std::unique_lock<std::mutex> lock(m_mutex);
-    if (m_cond_var.wait_for(lock, std::chrono::milliseconds(time_out_ms), [this]() { return !m_q.empty(); }))
+    // CODE_REVIEW: use unique_lock with constructor with now + time_out_ms
+    std::chrono::time_point timeout_point =
+        std::chrono::steady_clock::now() + time_out_ms;
+    std::unique_lock<std::timed_mutex> lock(m_mutex, timeout_point);
+    if (!lock.owns_lock())
+    {
+        return false;
+    }
+
+    if (m_cond_var.wait_until(lock, timeout_point,
+                              [this]() { return !m_q.empty(); }))
     {
         out_param = m_q.front();
         m_q.pop();
@@ -69,39 +84,8 @@ bool WaitableQueue<T, CONTAINER>::Pop(std::size_t time_out_ms, T& out_param)
 template <typename T, class CONTAINER>
 bool WaitableQueue<T, CONTAINER>::IsEmpty()
 {
-    std::unique_lock<std::mutex> lock(m_mutex);
+    std::unique_lock<std::timed_mutex> lock(m_mutex);
     return m_q.empty();
 }
-
 } // namespace ilrd
 #endif /* _ILRD_WAITABLEQUEUE_HPP */
-
-/*
-
-template<T,CONTAINER=std::queue>
-class WaitableQueue
-{
-public:
-    Push
-    void Pop(T& out_param)
-    bool Pop(time_out_ms,T& out_param)
-
-    bool IsEmpty();//חסר משמעות לכאורה
-
-private:
-    CONTAINER m_q;
-};
-
-template<T,PQ_CONTAINER=std::vector,COMPARE_FUNC=std::less>
-class PriorityQueue : private std::priority_queue<T,PQ_CONTAINER,COMPARE_FUNC>
-{
-public:
-    // API like std::queue
-    pop
-    push
-    front(){parent::top();}
-    IsEmpty
-};
-
-
-*/
--- cpp/test/WaitableQueueTest.cpp
+++ cpp/test/WaitableQueueTest.cpp
@@ -1,54 +1,168 @@
 /**************************************************************
  * File    : WaitableQueueTest.cpp
  * Author  : Ayal Moran
- * Reviewer: 
- * Date    : 
-**************************************************************/
+ * Reviewer:
+ * Date    :
+ **************************************************************/
 
-#include <iostream>
-
-#include "WaitableQueue.hpp"
-#include "test_utils.hpp"
-
-static void RegisterTests(void);
-
-/******************************************************
- * START OF ACTUAL TESTS
- ******************************************************/
-static void Test_Create(void)
-{
-    INIT_SUITE(create, "CREATE");
-
-    data_structure_t *ds = DSCreate();
-
-    RUN_TEST(create, "Create returns non-NULL", ds != NULL);
-
-    DSDestroy(ds);
-
-   std::cout << "== " <<  create.name << create.passed << "/" << create.total << " Passed ==" << std::endl;
-}
-
-int main(void)
-{
-    int i=0;
-    
-    PRINT_TEST_HEADER("WaitableQueue");
-    printf("===================\n");
-
-    RegisterTests();
-
-    for (i = 0; i < test_count; ++i)
-    {
-        std::cout << "Running Suite: "  <<  test_registry[i].name << std:endl;
-        test_registry[i].func();
-    }
-
-    PRINT_SUMMARY();
-    
-    return 0;
-}
-
-static void RegisterTests(void)
-{
-    REGISTER_TEST(Test_Create);
-}
+ #include <atomic>
+ #include <chrono>
+ #include <iostream>
+ #include <thread>
+ 
+ #include "WaitableQueue.hpp"
+ #include "PriorityQueue.hpp"
+ #include "test_utils.hpp"
+ 
+ using namespace std::chrono;
+ 
+ static void RegisterTests(void);
+ 
+ static void TestStartsEmpty(void)
+ {
+     INIT_SUITE(suite, "Starts Empty");
+     BEGIN_SUITE(suite);
+ 
+     ilrd::WaitableQueue<int> queue;
+     RUN_TEST(suite, "is empty", queue.IsEmpty());
+ 
+     END_SUITE(suite);
+     PRINT_SUITE_SUMMARY(suite);
+ }
+ 
+ static void TestPushPopSingle(void)
+ {
+     INIT_SUITE(suite, "Push Pop Single");
+     BEGIN_SUITE(suite);
+ 
+     ilrd::WaitableQueue<int> queue;
+     int value = 0;
+ 
+     queue.Push(42);
+     RUN_TEST(suite, "not empty", !queue.IsEmpty());
+ 
+     queue.Pop(value);
+     RUN_TEST(suite, "value is 42", value == 42);
+     RUN_TEST(suite, "empty again", queue.IsEmpty());
+ 
+     END_SUITE(suite);
+     PRINT_SUITE_SUMMARY(suite);
+ }
+ 
+ static void TestPriorityOrderWithPriorityContainer(void)
+ {
+     INIT_SUITE(suite, "Priority Container");
+     BEGIN_SUITE(suite);
+ 
+     ilrd::WaitableQueue<int, ilrd::PriorityQueue<int> > queue;
+     int value = 0;
+ 
+     queue.Push(3);
+     queue.Push(9);
+     queue.Push(5);
+ 
+     queue.Pop(value);
+     RUN_TEST(suite, "first is max", value == 9);
+ 
+     queue.Pop(value);
+     RUN_TEST(suite, "second is next max", value == 5);
+ 
+     queue.Pop(value);
+     RUN_TEST(suite, "third is last", value == 3);
+ 
+     END_SUITE(suite);
+     PRINT_SUITE_SUMMARY(suite);
+ }
+ 
+ static void TestPopBlocksUntilPush(void)
+ {
+     INIT_SUITE(suite, "Blocking Pop");
+     BEGIN_SUITE(suite);
+ 
+     ilrd::WaitableQueue<int> queue;
+     std::atomic<bool> pop_done(false);
+     int popped = 0;
+ 
+     std::thread consumer([&]() {
+         queue.Pop(popped);
+         pop_done = true;
+     });
+ 
+     std::this_thread::sleep_for(milliseconds(80));
+     RUN_TEST(suite, "pop is blocked", !pop_done);
+ 
+     queue.Push(99);
+     consumer.join();
+ 
+     RUN_TEST(suite, "pop completed", pop_done);
+     RUN_TEST(suite, "value is 99", popped == 99);
+     RUN_TEST(suite, "empty after pop", queue.IsEmpty());
+ 
+     END_SUITE(suite);
+     PRINT_SUITE_SUMMARY(suite);
+ }
+ 
+ static void TestProducerConsumer(void)
+ {
+     INIT_SUITE(suite, "Producer Consumer");
+     BEGIN_SUITE(suite);
+ 
+     ilrd::WaitableQueue<int> queue;
+     std::atomic<long long> sum(0);
+     const int kCount = 1000;
+     const long long expected = (static_cast<long long>(kCount) * (kCount + 1)) / 2;
+ 
+     std::thread producer([&]() {
+         for (int i = 1; i <= kCount; ++i)
+         {
+             queue.Push(i);
+         }
+     });
+ 
+     std::thread consumer([&]() {
+         int value = 0;
+         for (int i = 1; i <= kCount; ++i)
+         {
+             queue.Pop(value);
+             sum += value;
+         }
+     });
+ 
+     producer.join();
+     consumer.join();
+ 
+     RUN_TEST(suite, "sum matches", sum == expected);
+     RUN_TEST(suite, "queue empty", queue.IsEmpty());
+ 
+     END_SUITE(suite);
+     PRINT_SUITE_SUMMARY(suite);
+ }
+ 
+ int main(void)
+ {
+     PRINT_TEST_HEADER("WaitableQueue");
+     std::cout << "===================\n";
+ 
+     RegisterTests();
+ 
+     for (int i = 0; i < TestUtils::GetRegisteredTestCount(); ++i)
+     {
+         std::cout << "Running Suite: " << TestUtils::GetRegisteredTestName(i)
+                   << std::endl;
+         TestUtils::RunRegisteredTest(i);
+     }
+ 
+     PRINT_SUMMARY();
+ 
+     return (TestUtils::GetPassedTests() == TestUtils::GetTotalTests()) ? 0 : 1;
+ }
+ 
+ static void RegisterTests(void)
+ {
+     REGISTER_TEST(TestStartsEmpty);
+     REGISTER_TEST(TestPushPopSingle);
+     REGISTER_TEST(TestPriorityOrderWithPriorityContainer);
+     REGISTER_TEST(TestPopBlocksUntilPush);
+     REGISTER_TEST(TestProducerConsumer);
+ }
+ 
