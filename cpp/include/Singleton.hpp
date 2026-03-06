824c3b6e130b6e3163bf2b45d7ba12aae153b0b7
ayal.moran <moranayal@gmail.com>
2026-03-06 16:28:58 +0200

    made Singleton portable

--- cpp/include/Singleton.hpp
+++ cpp/include/Singleton.hpp
@@ -8,17 +8,17 @@
 #ifndef ILRD_SINGLETON_HPP
 #define ILRD_SINGLETON_HPP
 
-#include <cstdlib>  // std::atexit
+#include <cstdlib> // std::atexit
+
+#if __cplusplus >= 201103L
 
 namespace ilrd
 {
 
-/** @brief Lazy, thread-safe singleton for type T. */
 template <typename T>
 class Singleton
 {
   public:
-    /** @brief Get the single instance of T. */
     static T* GetInstance();
 
   private:
@@ -27,7 +27,6 @@ class Singleton
     Singleton(const Singleton&) = delete;
     Singleton& operator=(const Singleton&) = delete;
 
-    /** @brief Deletes the instance at process exit. */
     static void AtExit();
 
     static T* m_instance;
@@ -56,5 +55,93 @@ void Singleton<T>::AtExit()
 }
 
 } // namespace ilrd
+
+#else
+
+#include <pthread.h> // pthread_mutex_t, pthread_mutex_* API
+#include "ScopeLock.hpp"
+
+namespace ilrd
+{
+
+template <typename T>
+class Singleton
+{
+  public:
+    static T* GetInstance();
+
+  private:
+    Singleton();
+    ~Singleton();
+    Singleton(const Singleton&);
+    Singleton& operator=(const Singleton&);
+
+    class Mutex
+    {
+      public:
+        Mutex()
+        {
+            pthread_mutex_init(&m_mutex, 0);
+        }
+
+        ~Mutex()
+        {
+            pthread_mutex_destroy(&m_mutex);
+        }
+
+        void lock()
+        {
+            pthread_mutex_lock(&m_mutex);
+        }
+
+        void unlock()
+        {
+            pthread_mutex_unlock(&m_mutex);
+        }
+
+      private:
+        Mutex(const Mutex&);
+        Mutex& operator=(const Mutex&);
+
+        pthread_mutex_t m_mutex;
+    };
+
+    static void AtExit();
+
+    static T* m_instance;
+    static Mutex m_mutex;
+};
+
+template <typename T>
+T* Singleton<T>::m_instance = 0;
+
+template <typename T>
+typename Singleton<T>::Mutex Singleton<T>::m_mutex;
+
+template <typename T>
+T* Singleton<T>::GetInstance()
+{
+    ScopeLock<Mutex> guard(m_mutex);
+
+    if (0 == m_instance)
+    {
+        m_instance = new T;
+        std::atexit(&Singleton<T>::AtExit);
+    }
+
+    return m_instance;
+}
+
+template <typename T>
+void Singleton<T>::AtExit()
+{
+    ScopeLock<Mutex> guard(m_mutex);
+    delete m_instance;
+    m_instance = 0;
+}
+
+} // namespace ilrd
+
+#endif // __cplusplus
 
 #endif // ILRD_SINGLETON_HPP
