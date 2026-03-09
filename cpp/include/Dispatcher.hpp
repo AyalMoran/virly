41f391e5f07b416106d375e26898f88229bd9b07
ayal.moran <moranayal@gmail.com>
2026-03-09 16:49:06 +0200

    batman

---
+++ cpp/include/Dispatcher.hpp
@@ -0,0 +1,12 @@
+/**************************************************************
+ * File    : Dispatcher.hpp
+ * Author  : Ayal Moran
+ * Reviewer: 
+ * Date    : 
+**************************************************************/
+#ifndef _ILRD_DISPATCHER_HPP
+#define _ILRD_DISPATCHER_HPP
+
+/*Declarations for Dispatcher*/
+
+#endif /* _ILRD_DISPATCHER_HPP */
