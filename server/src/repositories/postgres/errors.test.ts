8fc3387a7a4891e4d032316542a5ee0118a38b39
AyalMoran <moranayal@gmail.com>
2026-06-24 09:47:16 +0300

    test(pg): cover Drizzle-wrapped 23505 cause in mapPgError (Task 5 review)

--- server/src/repositories/postgres/errors.test.ts
+++ server/src/repositories/postgres/errors.test.ts
@@ -12,6 +12,12 @@ test("mapPgError converts 23505 to DuplicateKeyError", () => {
   assert.throws(() => mapPgError(e, "email"), (x: unknown) => x instanceof DuplicateKeyError);
 });
 
+test("mapPgError converts a Drizzle-wrapped 23505 (on .cause) to DuplicateKeyError", () => {
+  const pgErr = Object.assign(new Error("dup"), { code: "23505", constraint: "users_email_uq" });
+  const drizzleErr = Object.assign(new Error("drizzle wrap"), { cause: pgErr });
+  assert.throws(() => mapPgError(drizzleErr, "email"), (x: unknown) => x instanceof DuplicateKeyError);
+});
+
 test("mapPgError rethrows other errors unchanged", () => {
   const e = new Error("other");
   assert.throws(() => mapPgError(e, "email"), (x: unknown) => x === e);
